6b27184645e776ca899b18f5bd7960c3c1a077ab
Pule-Taunyane <Pule.taunyane98@gmail.com>
2023-04-05 13:16:04 +0200

    Add project files.

---
+++ Re Captch/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Re_Captch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.confrimbtn = new System.Windows.Forms.Button();
+            this.classRoom2 = new System.Windows.Forms.PictureBox();
+            this.classRoom3 = new System.Windows.Forms.PictureBox();
+            this.classRoom7 = new System.Windows.Forms.PictureBox();
+            this.classRoom8 = new System.Windows.Forms.PictureBox();
+            this.classRoom9 = new System.Windows.Forms.PictureBox();
+            this.classRoom4 = new System.Windows.Forms.PictureBox();
+            this.classRoom5 = new System.Windows.Forms.PictureBox();
+            this.classRoom6 = new System.Windows.Forms.PictureBox();
+            this.classRoom1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Are You A Robot?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select The Computers To Gain Acess";
+            // 
+            // confrimbtn
+            // 
+            this.confrimbtn.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confrimbtn.Location = new System.Drawing.Point(99, 319);
+            this.confrimbtn.Name = "confrimbtn";
+            this.confrimbtn.Size = new System.Drawing.Size(212, 36);
+            this.confrimbtn.TabIndex = 11;
+            this.confrimbtn.Text = "Confirm";
+            this.confrimbtn.UseVisualStyleBackColor = true;
+            this.confrimbtn.Click += new System.EventHandler(this.confrimbtn_Click);
+            // 
+            // classRoom2
+            // 
+            this.classRoom2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom2.Image = global::Re_Captch.Properties.Resources.image_part_002;
+            this.classRoom2.Location = new System.Drawing.Point(159, 131);
+            this.classRoom2.Name = "classRoom2";
+            this.classRoom2.Size = new System.Drawing.Size(100, 50);
+            this.classRoom2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom2.TabIndex = 10;
+            this.classRoom2.TabStop = false;
+            this.classRoom2.Click += new System.EventHandler(this.classRoom2_Click);
+            // 
+            // classRoom3
+            // 
+            this.classRoom3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom3.Image = global::Re_Captch.Properties.Resources.image_part_003;
+            this.classRoom3.Location = new System.Drawing.Point(265, 131);
+            this.classRoom3.Name = "classRoom3";
+            this.classRoom3.Size = new System.Drawing.Size(100, 50);
+            this.classRoom3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom3.TabIndex = 9;
+            this.classRoom3.TabStop = false;
+            this.classRoom3.Click += new System.EventHandler(this.classRoom3_Click);
+            // 
+            // classRoom7
+            // 
+            this.classRoom7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom7.Image = global::Re_Captch.Properties.Resources.image_part_007;
+            this.classRoom7.Location = new System.Drawing.Point(53, 243);
+            this.classRoom7.Name = "classRoom7";
+            this.classRoom7.Size = new System.Drawing.Size(100, 50);
+            this.classRoom7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom7.TabIndex = 8;
+            this.classRoom7.TabStop = false;
+            this.classRoom7.Click += new System.EventHandler(this.classRoom7_Click);
+            // 
+            // classRoom8
+            // 
+            this.classRoom8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom8.Image = global::Re_Captch.Properties.Resources.image_part_008;
+            this.classRoom8.Location = new System.Drawing.Point(159, 243);
+            this.classRoom8.Name = "classRoom8";
+            this.classRoom8.Size = new System.Drawing.Size(100, 50);
+            this.classRoom8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom8.TabIndex = 7;
+            this.classRoom8.TabStop = false;
+            this.classRoom8.Click += new System.EventHandler(this.classRoom8_Click);
+            // 
+            // classRoom9
+            // 
+            this.classRoom9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom9.Image = global::Re_Captch.Properties.Resources.image_part_009;
+            this.classRoom9.Location = new System.Drawing.Point(265, 243);
+            this.classRoom9.Name = "classRoom9";
+            this.classRoom9.Size = new System.Drawing.Size(100, 50);
+            this.classRoom9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom9.TabIndex = 6;
+            this.classRoom9.TabStop = false;
+            this.classRoom9.Click += new System.EventHandler(this.classRoom9_Click);
+            // 
+            // classRoom4
+            // 
+            this.classRoom4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom4.Image = global::Re_Captch.Properties.Resources.image_part_004;
+            this.classRoom4.Location = new System.Drawing.Point(53, 187);
+            this.classRoom4.Name = "classRoom4";
+            this.classRoom4.Size = new System.Drawing.Size(100, 50);
+            this.classRoom4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom4.TabIndex = 5;
+            this.classRoom4.TabStop = false;
+            this.classRoom4.Click += new System.EventHandler(this.classRoom4_Click);
+            // 
+            // classRoom5
+            // 
+            this.classRoom5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom5.Image = global::Re_Captch.Properties.Resources.image_part_005;
+            this.classRoom5.Location = new System.Drawing.Point(159, 187);
+            this.classRoom5.Name = "classRoom5";
+            this.classRoom5.Size = new System.Drawing.Size(100, 50);
+            this.classRoom5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom5.TabIndex = 4;
+            this.classRoom5.TabStop = false;
+            this.classRoom5.Click += new System.EventHandler(this.classRoom5_Click);
+            // 
+            // classRoom6
+            // 
+            this.classRoom6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom6.Image = global::Re_Captch.Properties.Resources.image_part_006;
+            this.classRoom6.Location = new System.Drawing.Point(265, 187);
+            this.classRoom6.Name = "classRoom6";
+            this.classRoom6.Size = new System.Drawing.Size(100, 50);
+            this.classRoom6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom6.TabIndex = 3;
+            this.classRoom6.TabStop = false;
+            this.classRoom6.Click += new System.EventHandler(this.classRoom6_Click);
+            // 
+            // classRoom1
+            // 
+            this.classRoom1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classRoom1.Image = global::Re_Captch.Properties.Resources.image_part_001;
+            this.classRoom1.Location = new System.Drawing.Point(53, 131);
+            this.classRoom1.Name = "classRoom1";
+            this.classRoom1.Size = new System.Drawing.Size(100, 50);
+            this.classRoom1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.classRoom1.TabIndex = 2;
+            this.classRoom1.TabStop = false;
+            this.classRoom1.Click += new System.EventHandler(this.classRoom1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 397);
+            this.Controls.Add(this.confrimbtn);
+            this.Controls.Add(this.classRoom2);
+            this.Controls.Add(this.classRoom3);
+            this.Controls.Add(this.classRoom7);
+            this.Controls.Add(this.classRoom8);
+            this.Controls.Add(this.classRoom9);
+            this.Controls.Add(this.classRoom4);
+            this.Controls.Add(this.classRoom5);
+            this.Controls.Add(this.classRoom6);
+            this.Controls.Add(this.classRoom1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.classRoom1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox classRoom6;
+        private System.Windows.Forms.PictureBox classRoom5;
+        private System.Windows.Forms.PictureBox classRoom4;
+        private System.Windows.Forms.PictureBox classRoom9;
+        private System.Windows.Forms.PictureBox classRoom8;
+        private System.Windows.Forms.PictureBox classRoom7;
+        private System.Windows.Forms.PictureBox classRoom3;
+        private System.Windows.Forms.PictureBox classRoom2;
+        private System.Windows.Forms.Button confrimbtn;
+        private System.Windows.Forms.PictureBox classRoom1;
+    }
+}
+
---
+++ Re Captch/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Re_Captch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void classRoom1_Click(object sender, EventArgs e)
+        {
+            classRoom1.Image = null;
+            classRoom1.BackColor = Color.Black;
+        }
+
+        private void classRoom2_Click(object sender, EventArgs e)
+        {
+            classRoom2.Image = null;
+            classRoom2.BackColor = Color.Black;
+        }
+
+        private void classRoom3_Click(object sender, EventArgs e)
+        {
+            classRoom3.Image = null;
+            classRoom3.BackColor = Color.Black;
+        }
+
+        private void classRoom4_Click(object sender, EventArgs e)
+        {
+            classRoom4.Image = null;
+            classRoom4.BackColor = Color.Black;
+        }
+
+        private void classRoom5_Click(object sender, EventArgs e)
+        {
+            classRoom5.Image = null;
+            classRoom5.BackColor = Color.Black;
+        }
+
+        private void classRoom6_Click(object sender, EventArgs e)
+        {
+            classRoom6.Image = null;
+            classRoom6.BackColor = Color.Black;
+        }
+
+        private void classRoom7_Click(object sender, EventArgs e)
+        {
+            classRoom7.Image = null;
+            classRoom7.BackColor = Color.Black;
+        }
+
+        private void classRoom8_Click(object sender, EventArgs e)
+        {
+            classRoom8.Image = null;
+            classRoom8.BackColor = Color.Black;
+        }
+
+        private void classRoom9_Click(object sender, EventArgs e)
+        {
+            classRoom9.Image = null;
+            classRoom9.BackColor = Color.Black;
+        }
+
+        private void confrimbtn_Click(object sender, EventArgs e)
+        {
+            if(classRoom4.Image == null && classRoom5.Image == null && classRoom6.Image == null && classRoom7.Image == null && classRoom8.Image == null && classRoom9.Image == null && classRoom1.Image != null && classRoom2.Image != null && classRoom3.Image != null)
+            {
+                MessageBox.Show("Access Granted");
+            }
+            else
+            {
+                MessageBox.Show("Access Denied");
+            }
+            Application.Exit();
+        }
+    }
+}
